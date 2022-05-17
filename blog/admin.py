94f838f906fa262c1ba7179939ee8eaeb3505c6f
Karin Oldbring <karin.oldbring@gmail.com>
2022-05-17 13:48:07 +0000

    Add Admin Recipe functions

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Recipe
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Recipe, Comments
 
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'author']
+    list_filter = ('status', 'created_on')
     summernote_fields = ('ingredients', 'instructions')
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'recipe', 'created_on')
+    list_filter = ('created_on', 'name')
+    search_fields = ('body', 'name')
