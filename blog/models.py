f9c144946bc1401a089df6e5922a005c8cd340e1
Karin Oldbring <karin.oldbring@gmail.com>
2022-05-17 07:16:46 +0000

    Add model for comments

--- blog/models.py
+++ blog/models.py
@@ -5,34 +5,43 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 STATUS = ((0, 'Draft'), (1, 'Published'))
 
-class Post(models.Model):
+class Recipe(models.Model):
     """
     Model for the recipe
     """
 
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
     updated_on = models.DateTimeField(auto_now=True)
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     cooking_time = models.IntegerField(default=1,
-        validators=[
-            MinValueValidator(1), 
+            validators=[
+            MinValueValidator(1),
             MaxValueValidator(120)
         ])
     ingredients = models.TextField()
     instructions = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
 
 
 class Meta:
+    """
+    Order the recipes in descending order
+    """
     ordering = ['-created_on']
 
     def __str__(self):
+        """
+        Returns a string representation of an object
+        """
         return self.title
     
     def number_of_likes(self):
+        """
+        Returns the number of likes on a post
+        """
         return self.likes.count()
