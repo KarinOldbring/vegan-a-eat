131048628678e3cdfd658574d854d8c55dcddb47
Karin Oldbring <karin.oldbring@gmail.com>
2022-05-20 10:43:42 +0000

    Add hero image

--- blog/views.py
+++ blog/views.py
@@ -15,17 +15,17 @@ class RecipeDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
-        comments = recipe.comments.order_by('creeated_on')
+        comments = recipe.comments.order_by('created_on')
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
             liked = True
         
         return render(
-            request, 
-            "recipe_detail.html", 
+            request,
+            "recipe_detail.html",
             {
-                "recipe": recipe, 
-                "comments": comments, 
+                "recipe": recipe,
+                "comments": comments,
                 "liked": liked
             },
         )
