eb79f0411c2670e7f5fe89700b3bcb5fa9c172f6
Karin Oldbring <karin.oldbring@gmail.com>
2022-05-23 10:05:56 +0000

    Add url for login

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
+    path('login/', views.login_page, name='login'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
-    path('', views.login_page, name='login'),
 ]
