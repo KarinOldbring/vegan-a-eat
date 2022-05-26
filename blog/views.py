48a5f48b76b4e86dfbbe2bdb3e990e59a41c7248
Karin Oldbring <karin.oldbring@gmail.com>
2022-05-26 12:56:45 +0000

    Add CommentForm to views.py

--- blog/views.py
+++ blog/views.py
@@ -60,6 +60,26 @@ class RecipeDetail(View):
                 "comment_form": CommentForm()
             },
         )
+    
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.order_by('created_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+            
+        return render(
+            request, "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
 
 def login_page(request):
     """
