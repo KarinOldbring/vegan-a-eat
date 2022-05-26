6dc313cb4ac89608e58c2453d6aca8112fc47717
Karin Oldbring <karin.oldbring@gmail.com>
2022-05-26 07:33:57 +0000

    Add url for about

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
+    path('about/', views.about, name='about'),
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('signup/', views.signup_page, name='signup'),
     path('addrecipe/', views.add_recipe, name='addrecipe'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
-    path('about/', views.about, name='about'),
 ]
